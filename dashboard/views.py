8a948e6ba1cb0e0419ff4a59e3fd13ec26877b44
orges <yetkin85@gmail.com>
2014-02-16 22:05:46 +0200

    Create Issue model form

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import View
-from issue.models import (IssueType, IssueStatus, IssueFlow, IssuePriority,
-                          IssueTemplate)
-from project.models import ProjectVersion
+from django.forms import ModelForm
+from issue.models import Issue
+
+
+class CreateIssueForm(ModelForm):
+    class Meta:
+        model = Issue
+        fields = [
+            'title', 'summary', 'effort', 'project_version', 'type',
+            'status', 'priority', 'template', 'flow', 'sub_issues','due_date'
+        ]
+
 
 class MainDashboardView(View):
     template_name = 'dashboard/main.html'
 
     def get(self,request):
         content = {}
-        content['issue_type_list'] = IssueType.objects.all()
-        content['issue_status_list'] = IssueStatus.objects.all()
-        content['issue_priority_list'] = IssuePriority.objects.all()
-        content['issue_template_list'] = IssueTemplate.objects.all()
-        content['issue_flow_list'] = IssueFlow.objects.all()
-        content['project_version_list'] = ProjectVersion.objects.all()
+        content['create_issue_form'] = CreateIssueForm()
         return render(request, self.template_name, content)
 
     def post(self,request):
