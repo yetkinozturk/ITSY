d7950fd230a23ba728a0929668f49de98e324809
orges <yetkin85@gmail.com>
2014-02-18 22:00:02 +0200

    create issue sub views

--- issue/urls.py
+++ issue/urls.py
@@ -1,8 +1,21 @@
 from django.conf.urls import patterns, include, url
 
-from issue.views import CreateIssueView
+from issue.views import (CreateIssueView,CreateIssueTypeView,CreateIssueStatusView,
+                         CreateIssuePriorityView,CreateIssueCharView,CreateIssueTextView,
+                         CreateIssueImageView,CreateIssueFileView,CreateIssuePersonView,
+                         CreateIssueFlowView,CreateIssueTemplateView)
 
 urlpatterns = patterns('',
     #url(r'^$', MainDashboardView.as_view(), name='home'),
     url(r'^create/', CreateIssueView.as_view(),name='create_issue'),
+    url(r'^createissuetype/', CreateIssueTypeView.as_view(),name='create_issue'),
+    url(r'^createissuestatus/', CreateIssueStatusView.as_view(),name='create_issue'),
+    url(r'^createissuepriority/', CreateIssuePriorityView.as_view(),name='create_issue'),
+    url(r'^createissuechar/', CreateIssueCharView.as_view(),name='create_issue'),
+    url(r'^createissuetext/', CreateIssueTextView.as_view(),name='create_issue'),
+    url(r'^createissueimage/', CreateIssueImageView.as_view(),name='create_issue'),
+    url(r'^createissuefile/', CreateIssueFileView.as_view(),name='create_issue'),
+    url(r'^createissueperson/', CreateIssuePersonView.as_view(),name='create_issue'),
+    url(r'^createissueflow/', CreateIssueFlowView.as_view(),name='create_issue'),
+    url(r'^createissuetemplate/', CreateIssueTemplateView.as_view(),name='create_issue'),
 )
--- issue/views.py
+++ issue/views.py
@@ -2,7 +2,7 @@ from django.core.context_processors import csrf
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.forms import ModelForm
-from issue.models import Issue
+from issue.models import (Issue, IssueType)
 
 
 class CreateIssueForm(ModelForm):
@@ -21,6 +21,27 @@ class CreateIssueView(View):
     def get(self,request):
         self.content.update(csrf(request))
         self.content['create_issue_form'] = CreateIssueForm()
+        return render(request, self.template_name, self.content)
+
+    def post(self,request):
+        return render(request, self.template_name)
+
+
+class CreateIssueTypeForm(ModelForm):
+    class Meta:
+        model = IssueType
+        fields = [
+            'name',
+        ]
+
+
+class CreateIssueTypeView(View):
+    template_name = 'issue/createissuetype.html'
+    content = {}
+
+    def get(self,request):
+        self.content.update(csrf(request))
+        self.content['create_issue_type_form'] = CreateIssueTypeForm()
         return render(request, self.template_name, self.content)
 
     def post(self,request):
