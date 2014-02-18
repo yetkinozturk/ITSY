78d62e8a6e7bcea9d251756c170650041e32efc9
orges <yetkin85@gmail.com>
2014-02-18 22:35:36 +0200

    issue type templates

--- itsy/settings.py
+++ itsy/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
     'taggit',
     'account',
     'common',
