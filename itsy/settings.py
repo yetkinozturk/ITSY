83cee88cc89e5161c613db8832f11aa6d01ea388
orges <yetkin85@gmail.com>
2014-02-19 20:27:32 +0200

    better bootstrap support

--- itsy/settings.py
+++ itsy/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_forms_bootstrap',
+    'bootstrap_toolkit',
     'taggit',
     'account',
     'common',
