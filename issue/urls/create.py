4db080f89ee8ebd05d6570bf4d7d6916c4fb789d
orges <yetkin85@gmail.com>
2014-02-22 15:49:17 +0200

    refactor issue create views/templates

--- issue/urls/create.py
+++ issue/urls/create.py
@@ -1,21 +1,102 @@
 from django.conf.urls import patterns, url
-
-from issue.views import (CreateIssueView,CreateIssueTypeView,CreateIssueStatusView,
-                         CreateIssuePriorityView,CreateIssueCharView,CreateIssueTextView,
-                         CreateIssueImageView,CreateIssueFileView,CreateIssuePersonView,
-                         CreateIssueFlowView,CreateIssueTemplateView)
+from issue.models import (IssueType, IssueStatus, IssuePriority,IssueCharField,
+                          IssueTextField, IssueImageField,IssueFileField,
+                          IssuePerson, IssueFlow, IssueTemplate, Issue)
+from issue.views import (CreateIssueView, CreateIssueFlow, CreateIssueField)
 
 
 urlpatterns = patterns('',
-    url(r'^type/',       CreateIssueTypeView.as_view(),      name='type'),
-    url(r'^status/',     CreateIssueStatusView.as_view(),    name='status'),
-    url(r'^priority/',   CreateIssuePriorityView.as_view(),  name='priority'),
-    url(r'^char/',       CreateIssueCharView.as_view(),      name='char'),
-    url(r'^text/',       CreateIssueTextView.as_view(),      name='text'),
-    url(r'^image/',      CreateIssueImageView.as_view(),     name='image'),
-    url(r'^file/',       CreateIssueFileView.as_view(),      name='file'),
-    url(r'^person/',     CreateIssuePersonView.as_view(),    name='person'),
-    url(r'^flow/',       CreateIssueFlowView.as_view(),      name='flow'),
-    url(r'^template/',   CreateIssueTemplateView.as_view(),  name='template'),
-    url(r'^',            CreateIssueView.as_view(),          name='item'),
+
+    url(r'^type/',
+        CreateIssueField.as_view(
+            model=IssueType,
+            page_title='ITSY Create Issue Type',
+            page_heading='Create An Issue Type'
+        ),
+        name='type'),
+
+    url(r'^status/',
+        CreateIssueField.as_view(
+            model=IssueStatus,
+            page_title='ITSY Create Issue Status',
+            page_heading='Create An Issue Status'
+        ),
+        name='status'),
+
+    url(r'^priority/',
+        CreateIssueField.as_view(
+            model=IssuePriority,
+            page_title='ITSY Create Issue Priority',
+            page_heading='Create An Issue Priority'
+        ),
+        name='priority'),
+
+    url(r'^char/',
+        CreateIssueField.as_view(
+            model=IssueCharField,
+            page_title='ITSY Create Issue Text Field',
+            page_heading='Create An Issue Text Field'
+        ),
+        name='char'),
+
+    url(r'^text/',
+        CreateIssueField.as_view(
+            model=IssueTextField,
+            page_title='ITSY Create Issue Text Area Field',
+            page_heading='Create An Issue Text Area Field'
+        ),
+        name='text'),
+
+    url(r'^image/',
+        CreateIssueField.as_view(
+            model=IssueImageField,
+            page_title='ITSY Create Issue Image Field',
+            page_heading='Create An Issue Image Field'
+        ),
+        name='image'),
+
+    url(r'^file/',
+        CreateIssueField.as_view(
+            model=IssueFileField,
+            page_title='ITSY Create Issue File Field',
+            page_heading='Create An Issue File Field'
+        ),
+        name='file'),
+
+    url(r'^person/',
+        CreateIssueField.as_view(
+            model=IssuePerson,
+            page_title='ITSY Create Issue Person Field',
+            page_heading='Create An Issue Person Field'
+        ),name='person'),
+
+    url(r'^flow/',
+        CreateIssueFlow.as_view(
+            model=IssueFlow,
+            template_name = 'issue/create/issueflow.html'
+        ),
+        name='flow'),
+
+    url(r'^template/',
+        CreateIssueField.as_view(
+            model=IssueTemplate,
+            fields = [
+                'name','char_fields','text_fields','image_fields','file_fields',
+                'people','project',
+            ],
+            page_title='ITSY Create Issue Template',
+            page_heading='Create An Issue Template'
+        ),
+        name='template'),
+
+    url(r'^',
+        CreateIssueView.as_view(
+            model=Issue,
+            fields = [
+                'title', 'summary', 'effort', 'project_version', 'type',
+                'status', 'priority', 'template', 'flow', 'sub_issues','due_date'
+            ],
+            template_name = 'issue/create/issue.html'
+        ),
+        name='item'),
 )
