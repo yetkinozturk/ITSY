cced3bb8b525993a3f31621377ac20aa2ab28af9
orges <yetkin85@gmail.com>
2014-02-22 02:40:43 +0200

    success urls for project create views

--- project/urls/create.py
+++ project/urls/create.py
@@ -8,34 +8,39 @@ urlpatterns = patterns('',
     url(r'^project/',
         CreateView.as_view(
             model=Project,
-            template_name='project/create/project.html'
+            template_name='project/create/project.html',
+            success_url='/project/view/project/'
         ),
         name='project'),
 
     url(r'^category/',
         CreateView.as_view(
             model=ProjectCategory,
-            template_name='project/create/category.html'
+            template_name='project/create/category.html',
+            success_url='/project/view/category/'
         ),
         name='category'),
 
     url(r'^version/',
         CreateView.as_view(
             model=ProjectVersion,
-            template_name='project/create/version.html'
+            template_name='project/create/version.html',
+            success_url='/project/view/version/'
         ),
         name='version'),
 
     url(r'^milestone/',
         CreateView.as_view(
             model=Milestone,
-            template_name='project/create/milestone.html'
+            template_name='project/create/milestone.html',
+            success_url='/project/view/milestone/'
         ),
         name='milestone'),
 
     url(r'^milestatus/',
         CreateView.as_view(
             model=MilestoneStatus,
-            template_name='project/create/milestatus.html'
+            template_name='project/create/milestatus.html',
+            success_url='/project/view/milestatus/'
         ),    name='milestatus'),
 )
