67e5eaaf21c991e82f6ae54a847ed10eca6dc2ec
orges <yetkin85@gmail.com>
2014-02-23 22:10:41 +0200

    issue detail update view

--- issue/views.py
+++ issue/views.py
@@ -2,14 +2,16 @@ from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
-from django.core.urlresolvers import reverse
-from django.utils.translation import ugettext_lazy as _
 from django import forms
-from issue.models import Issue
+from issue.models import (Issue, IssueCharValue, IssueTextValue, IssueImageValue,
+                          IssueFileValue, IssuePersonValue)
 from account.models import Account
 
 
 class CreateIssueDetailsForm(forms.ModelForm):
+    original_fields = {}
+    field_value_class = {}
+    field_type_instance = {}
     class Meta:
         model = Issue
 
@@ -20,28 +22,80 @@ class CreateIssueDetailsForm(forms.ModelForm):
         if issue:
             self.fields.pop('effort_calc')
             self.fields.pop('template')
+            self.original_fields = self.fields.copy()
 
             if issue.template:
 
                 char_fields = issue.template.char_fields.all()
                 for field in char_fields:
                     self.fields[field.name] = forms.CharField(max_length=255, required=field.required)
+                    self.field_value_class[field.name] = IssueCharValue
+                    self.field_type_instance[field.name] = field
+                    try:
+                        item = IssueCharValue.objects.get(issue=issue,field=field)
+                        self.fields[field.name].initial = item.value
+                    except IssueCharValue.DoesNotExist:
+                        pass
 
                 text_fields = issue.template.text_fields.all()
                 for field in text_fields:
                     self.fields[field.name] = forms.CharField(widget=forms.Textarea, required=field.required)
+                    self.field_value_class[field.name] = IssueTextValue
+                    self.field_type_instance[field.name] = field
+                    try:
+                        item = IssueTextValue.objects.get(issue=issue,field=field)
+                        self.fields[field.name].initial = item.value
+                    except IssueTextValue.DoesNotExist:
+                        pass
 
                 image_fields = issue.template.image_fields.all()
                 for field in image_fields:
                     self.fields[field.name] = forms.ImageField(required=field.required)
+                    self.field_value_class[field.name] = IssueImageValue
+                    self.field_type_instance[field.name] = field
+                    try:
+                        item = IssueImageValue.objects.get(issue=issue,field=field)
+                        self.fields[field.name].initial = item.value
+                    except IssueImageValue.DoesNotExist:
+                        pass
 
                 file_fields = issue.template.file_fields.all()
                 for field in file_fields:
                     self.fields[field.name] = forms.FileField(required=field.required)
+                    self.field_value_class[field.name] = IssueFileValue
+                    self.field_type_instance[field.name] = field
+                    try:
+                        item = IssueFileValue.objects.get(issue=issue,field=field)
+                        self.fields[field.name].initial = item.value
+                    except IssueFileValue.DoesNotExist:
+                        pass
 
                 people = issue.template.people.all()
                 for field in people:
                     self.fields[field.role] = forms.ModelChoiceField(queryset=Account.objects.all(),required=field.required)
+                    self.field_value_class[field.role] = IssuePersonValue
+                    self.field_type_instance[field.role] = field
+                    try:
+                        item = IssuePersonValue.objects.get(issue=issue,field=field)
+                        self.fields[field.role].initial = item.value
+                    except IssuePersonValue.DoesNotExist:
+                        pass
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        m = super(CreateIssueDetailsForm, self).save(commit=False)
+        if commit:
+            m.save()
+        if len(self.field_value_class) > 0:
+            for k,v in self.original_fields.iteritems():
+                self.cleaned_data.pop(k)
+        for k,v in self.cleaned_data.iteritems():
+            item,created = self.field_value_class[k].objects.get_or_create(
+                issue=m,
+                field=self.field_type_instance.get(k))
+            item.value = self.cleaned_data.get(k)
+            item.save()
+        return m
+
 
 class CreateIssueDetails(UpdateView):
     slug = ''
