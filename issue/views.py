fd32c42eb394466f98e26b68217031d40283a1f5
orges <yetkin85@gmail.com>
2014-02-23 19:01:37 +0200

    issue detail view

--- issue/views.py
+++ issue/views.py
@@ -3,13 +3,26 @@ from django.views.generic.edit import UpdateView
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
 from django import forms
 from issue.models import Issue
 
 
+class CreateIssueDetailsForm(forms.ModelForm):
+    class Meta:
+        model = Issue
+
+    def __init__(self, *args, **kwargs):
+        super(CreateIssueDetailsForm, self).__init__(*args, **kwargs)
+        issue = kwargs.get('instance',None)
+        if issue:
+            self.fields.pop('effort_calc')
+            self.fields['profile'] = forms.CharField(max_length=200)
+
 class CreateIssueDetails(UpdateView):
     slug = ''
     context_object_name = 'issue'
+    form_class = CreateIssueDetailsForm
 
     def get(self, request, *args, **kwargs):
         self.slug = kwargs.get('slug')
