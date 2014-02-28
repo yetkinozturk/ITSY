85656b97f1092b413e6561e9df771b41ad4e949d
orges <yetkin85@gmail.com>
2014-02-28 23:48:13 +0200

    account model refactor

--- account/forms.py
+++ account/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import ugettext_lazy as _
 from account.models import Account
 
 
 class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Password confirmation", widget=forms.PasswordInput)
+    password1 = forms.CharField(label=_(u"Password"), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_(u"Password confirmation"), widget=forms.PasswordInput)
+    firstname = forms.CharField(label=_(u'First Name'),widget=forms.TextInput)
+    lastname = forms.CharField(label=_(u'Last Name'),widget=forms.TextInput)
     class Meta:
         model = Account
         fields = ("email","role","team")
@@ -17,9 +20,13 @@ class UserCreationForm(forms.ModelForm):
             raise forms.ValidationError("Password mismatch")
         return password2
 
+
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
         user.set_password(self.cleaned_data["password1"])
+        user.firstname = self.cleaned_data["firstname"]
+        user.lastname = self.cleaned_data["lastname"]
+
         if commit:
             user.save()
         return user
--- account/models.py
+++ account/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
@@ -18,18 +17,20 @@ class AccountTeam(models.Model):
 
 
 class AccountManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, firstname, lastname, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
         user = self.model(email=AccountManager.normalize_email(email))
-
+        user.firstname = firstname
+        user.lastname = lastname
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        u = self.create_user(email=email, password=password)
+    def create_superuser(self, email, firstname, lastname, password):
+        u = self.create_user(email=email,firstname=firstname,
+                             lastname=lastname, password=password)
         u.is_admin = True
         u.save(using=self._db)
         return u
@@ -41,6 +42,8 @@ class Account(AbstractBaseUser):
         ('AD', 'Admin'),
         ('US', 'User'),
     )
+    firstname = models.CharField(_(u'First Name'), max_length=128)
+    lastname = models.CharField(_(u'Last Name'), max_length=128)
     role = models.ForeignKey(AccountRole, verbose_name=_(u'Role'), null=True, blank=True)
     team = models.ForeignKey(AccountTeam, verbose_name=_(u'Team'), null=True, blank=True)
     type = models.CharField(_(u'Account Type'), max_length=4, choices=ACCOUNT_TYPES, default='US')
@@ -52,18 +55,15 @@ class Account(AbstractBaseUser):
     objects = AccountManager()
 
     USERNAME_FIELD = 'email'
-    #REQUIRED_FIELDS = ['email',]
+    REQUIRED_FIELDS = ['firstname','lastname']
 
     def __unicode__(self):
-        return u'%s %s' % (self.first_name, self.last_name)
+        return u'%s %s' % (self.firstname, self.lastname)
 
     def get_full_name(self):
-        return self.email
+        return u'%s %s' % (self.firstname, self.lastname)
 
     def get_short_name(self):
-        return self.email
-
-    def __unicode__(self):
         return self.email
 
     def has_perm(self, perm, obj=None):
