a086fc453da2ae2c462b73ea07395bb3974c590d
orges <yetkin85@gmail.com>
2014-03-07 23:36:27 +0200

    repos templates refactor

--- repos/tables.py
+++ repos/tables.py
@@ -5,7 +5,8 @@ from repos.models import CodeRepository
 class RepoTable(tables.Table):
     edit_entries = tables.TemplateColumn('<a href="/repos/edit/repos/{{record.id}}/" style="color:red">Edit</a>')
     delete_entries = tables.TemplateColumn('<a href="/repos/delete/repos/{{record.id}}/" style="color:red">Delete</a>')
-
+    commits = tables.TemplateColumn('<a href="/repos/{{record.slug}}/" style="color:red">Recent Commits</a>')
+    browse = tables.TemplateColumn('<a href="/repos/{{record.slug}}/browser/" style="color:red">Browse</a>')
     class Meta:
         model = CodeRepository
         attrs = {"class": "paleblue"}
