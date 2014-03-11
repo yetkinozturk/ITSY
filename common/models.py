46efeec9194be42f410d3016807476ffe2c0ddae
orges <yetkin85@gmail.com>
2014-03-11 21:35:49 +0200

    configuration model update

--- common/models.py
+++ common/models.py
@@ -16,15 +16,19 @@ class MainConfiguration(models.Model):
     """
     ## Mail Settings
     email_use_tls = models.BooleanField(_(u'EMAIL_USE_TLS'),default=True)
-    email_host = models.CharField(_(u'EMAIL_HOST'),max_length=1024)
-    email_host_user = models.CharField(_(u'EMAIL_HOST_USER'),max_length=255)
-    email_host_password = models.CharField(_(u'EMAIL_HOST_PASSWORD'),max_length=255)
-    email_port = models.PositiveSmallIntegerField(_(u'EMAIL_PORT'),default=587)
+    email_host = models.CharField(_(u'EMAIL_HOST'),max_length=1024,help_text=_(u'e.g: smtp.gmail.com'))
+    email_host_user = models.EmailField(_(u'EMAIL_HOST_USER'),max_length=255,help_text=_(u'e.g: youraccount@gmail.com'))
+    email_host_password = models.CharField(_(u'EMAIL_HOST_PASSWORD'),max_length=255,help_text=_(u'Mail account password'))
+    email_port = models.PositiveSmallIntegerField(_(u'EMAIL_PORT'),default=587,help_text=_(u'Email connection port e.g: 587'))
     email_fail_silently = models.BooleanField(_(u'EMAIL_FAIL_SILENTLY'), default=False)
+    notification_email = models.EmailField(_(u'Notification Account'),help_text=_(u'Email address for notifications'))
 
     ## Account Registration/Activation
     allow_registration = models.BooleanField(_(u'Allow Registration'),default=True,help_text=_(u'If disabled, Accounts need to be created internally'))
     active_after_registration = models.BooleanField(_(u'Accounts Active After Registration'),default=True,help_text=_(u'Enable Accounts after registration automatically'))
+
+    # Authorization
+    allow_delete_items = models.BooleanField(_(u'Allow Users to Delete'),default=False,help_text=_(u'Allow normal users to delete items.'))
 
     ## Notifications
     email_changes_to_reporter = models.BooleanField(_(u'Notify Reporter'),default=True,help_text=_(u'Send Notification to issue reporter for every issue change'))
